dd4ac4b33baeecd16845291ff11382df6e5c5840
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-10-10 11:57:59 -0700

    Init and import header on layout page

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Header from './components/Header'
 
 const inter = Inter({
   subsets: ['latin'],
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className={inter.className}>
+        <Header />
         <main className='mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8'>{children}</main>
       </body>
     </html>
