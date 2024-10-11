52fedb106684681a703643e2f986497460333fd7
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-10-11 12:42:55 -0700

    Add and display logo

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,3 +1,7 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import logo from '../../../public/logo.svg'
+
 const Header = () => {
   return (
     <header className='bg-gray-100'>
@@ -5,7 +9,7 @@ const Header = () => {
         <div class='flex h-16 items-center justify-between'>
           <div class='flex items-center'>
             <a href='/'>
-              <img class='h-12 w-12' src='images/logo.svg' alt='Bookit' />
+              <Image class='h-12 w-12' src={logo} alt='Instaroom Logo' priority={true} />
             </a>
             <div class='hidden md:block'>
               <div class='ml-10 flex items-baseline space-x-4'>
