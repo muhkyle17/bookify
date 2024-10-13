4cf316ddf3cfd38190a49814c17b8ab88c430cfb
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-10-13 12:53:21 -0700

    Use and display the room card component

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,16 @@
 import rooms from '@/data/rooms.json'
+import RoomCard from './components/RoomCard'
 
 export default function Home() {
   console.log(rooms, 'rooms')
 
   return (
-    <div>
-      <p>Instaroom App</p>
-    </div>
+    <>
+      {rooms.length > 0 ? (
+        rooms.map(room => <RoomCard key={room.$id} room={room} />)
+      ) : (
+        <p>No rooms available at the moment.</p>
+      )}
+    </>
   )
 }
