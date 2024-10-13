3224ca0d390809837675816320a47d5c184d17c4
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-10-13 13:13:16 -0700

    Display the heading component on the main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
 import rooms from '@/data/rooms.json'
 import RoomCard from './components/RoomCard'
+import Heading from './components/Heading'
 
 export default function Home() {
-  console.log(rooms, 'rooms')
-
   return (
     <>
+      <Heading title='Available Rooms' />
       {rooms.length > 0 ? (
         rooms.map(room => <RoomCard key={room.$id} room={room} />)
       ) : (
