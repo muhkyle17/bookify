d0170748c339237dfff6b9108d70f70b5bccf7df
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-10-13 13:40:54 -0700

    Find and display the room initially

--- src/app/rooms/[id]/page.jsx
+++ src/app/rooms/[id]/page.jsx
@@ -1,5 +1,21 @@
-const RoomPage = () => {
-  return <div>Room Page</div>
+import rooms from '@/data/rooms.json'
+import Heading from '@/src/app/components/Heading'
+
+const RoomPage = ({ params }) => {
+  const { id } = params
+  const room = rooms.find(room => room.$id === id)
+
+  console.log(room, 'room')
+
+  if (!room) {
+    return <Heading title='Room Not Found' />
+  }
+
+  return (
+    <>
+      <Heading title={room.name} />
+    </>
+  )
 }
 
 export default RoomPage
