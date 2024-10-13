418ca14f31e50160fe936b4898267e29bf827ed5
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-10-13 13:08:31 -0700

    Display important room details dynamically

--- src/app/components/RoomCard.jsx
+++ src/app/components/RoomCard.jsx
@@ -9,30 +9,28 @@ const RoomCard = ({ room }) => {
           src={`/images/rooms/${room.image}`}
           width={400}
           height={100}
-          alt='Grand Conference Hall'
+          alt={room.name}
           className='w-full sm:w-32 sm:h-32 mb-3 sm:mb-0 object-cover rounded-lg'
         />
         <div className='space-y-1'>
-          <h4 className='text-lg font-semibold'>Grand Conference Hall</h4>
+          <h4 className='text-lg font-semibold'>{room.name}</h4>
           <p className='text-sm text-gray-600'>
-            <span className='font-semibold text-gray-800'> Address:</span> 555 California St, San
-            Francisco, CA 94104
+            <span className='font-semibold text-gray-800'> Address:</span> ${room.address}
           </p>
           <p className='text-sm text-gray-600'>
-            <span className='font-semibold text-gray-800'> Availability:</span>9 AM - 5 PM
+            <span className='font-semibold text-gray-800'> Availability:</span>${room.availability}
           </p>
           <p className='text-sm text-gray-600'>
-            <span className='font-semibold text-gray-800'> Price:</span>
-            $150/hour
+            <span className='font-semibold text-gray-800'> Price:</span> ${room.price_per_hour}/hour
           </p>
         </div>
       </div>
       <div className='flex flex-col sm:flex-row w-full sm:w-auto sm:space-x-2 mt-2 sm:mt-0'>
-        <a
-          href='room.html'
+        <Link
+          href={`/rooms/${room.$id}`}
           className='bg-blue-500 text-white px-4 py-2 rounded mb-2 sm:mb-0 w-full sm:w-auto text-center hover:bg-blue-700'>
           View Room
-        </a>
+        </Link>
       </div>
     </div>
   )
