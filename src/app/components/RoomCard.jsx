674fd0f192cfb526f8a839f24c9302f5408ba828
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-10-13 12:55:46 -0700

    Import and display the images of all the rooms

--- src/app/components/RoomCard.jsx
+++ src/app/components/RoomCard.jsx
@@ -1,11 +1,14 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
-const RoomCard = () => {
+const RoomCard = ({ room }) => {
   return (
     <div className='bg-white shadow rounded-lg p-4 mt-4 flex flex-col sm:flex-row justify-between items-start sm:items-center'>
       <div className='flex flex-col sm:flex-row sm:space-x-4'>
-        <img
-          src='images/rooms/room-1.jpg'
+        <Image
+          src={`/images/rooms/${room.image}`}
+          width={400}
+          height={100}
           alt='Grand Conference Hall'
           className='w-full sm:w-32 sm:h-32 mb-3 sm:mb-0 object-cover rounded-lg'
         />
