7d148ed3100a164fa4fd03bcf65eeee20aba5914
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-10-12 20:11:17 -0700

    Change all instances of a tags to link tags

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,52 +1,53 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import logo from '../../../public/logo.svg'
+import { FaUser, FaSignInAlt, FaSignOutAlt, FaBuilding } from 'react-icons/fa'
 
 const Header = () => {
   return (
     <header className='bg-gray-100'>
-      <nav class='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
+      <nav class='mx-auto max-w-7xl px-4 s  m:px-6 lg:px-8'>
         <div class='flex h-16 items-center justify-between'>
           <div class='flex items-center'>
-            <a href='/'>
+            <Link href='/'>
               <Image class='h-12 w-12' src={logo} alt='Instaroom Logo' priority={true} />
-            </a>
+            </Link>
             <div class='hidden md:block'>
               <div class='ml-10 flex items-baseline space-x-4'>
-                <a
+                <Link
                   href='/'
                   class='rounded-md px-3 py-2 text-sm font-medium text-gray-800 hover:bg-gray-700 hover:text-white'>
                   Rooms
-                </a>
+                </Link>
 
-                <a
+                <Link
                   href='/bookings.html'
                   class='rounded-md px-3 py-2 text-sm font-medium text-gray-800 hover:bg-gray-700 hover:text-white'>
                   Bookings
-                </a>
-                <a
+                </Link>
+                <Link
                   href='/add-room.html'
                   class='rounded-md px-3 py-2 text-sm font-medium text-gray-800 hover:bg-gray-700 hover:text-white'>
                   Add Room
-                </a>
+                </Link>
               </div>
             </div>
           </div>
 
           <div class='ml-auto'>
             <div class='ml-4 flex items-center md:ml-6'>
-              <a href='login.html' class='mr-3 text-gray-800 hover:text-gray-600'>
+              <Link href='login.html' class='mr-3 text-gray-800 hover:text-gray-600'>
                 <i class='fa fa-sign-in'></i> Login
-              </a>
-              <a href='register.html' class='mr-3 text-gray-800 hover:text-gray-600'>
+              </Link>
+              <Link href='register.html' class='mr-3 text-gray-800 hover:text-gray-600'>
                 <i class='fa fa-user'></i> Register
-              </a>
-              <a href='my-rooms.html'>
+              </Link>
+              <Link href='my-rooms.html'>
                 <i class='fa fa-building'></i> My Rooms
-              </a>
-              <a href='login.html' class='mx-3 text-gray-800 hover:text-gray-600'>
+              </Link>
+              <Link href='login.html' class='mx-3 text-gray-800 hover:text-gray-600'>
                 <i class='fa fa-sign-out'></i> Sign Out
-              </a>
+              </Link>
             </div>
           </div>
         </div>
