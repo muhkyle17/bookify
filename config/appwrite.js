a0c8501712e1beab8ebe6d64f585ea35517f4957
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-10-15 12:50:58 -0700

    Add session client and admin client

--- config/appwrite.js
+++ config/appwrite.js
@@ -1,1 +1,46 @@
 import { Client, Databases, Account, Storage } from 'node-appwrite'
+
+// Admin Client
+const createAdminClient = async () => {
+  const client = new Client()
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT)
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT)
+    .setKey(process.env.NEXT_APPWRITE_KEY)
+
+  console.log(Account, 'Account') // !TEMPORARY
+  console.log(Databases, 'Databases') // !TEMPORARY
+  console.log(Storage, 'Storage') // !TEMPORARY
+
+  return {
+    get account() {
+      return new Account(client)
+    },
+    get databases() {
+      return new Databases(client)
+    },
+    get storage() {
+      return new Storage(client)
+    },
+  }
+}
+
+const createSessionClient = async session => {
+  const client = new Client()
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT)
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT)
+
+  if (session) {
+    client.setSession(session)
+  }
+
+  return {
+    get account() {
+      return new Account(client)
+    },
+    get databases() {
+      return new Databases(client)
+    },
+  }
+}
+
+export { createAdminClient, createSessionClient }
