04dc2f3cf521b26f88aaaed6f978b9893faf2658
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-10-19 15:16:51 -0700

    Add logic for using action with create session

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,10 +1,11 @@
 import Link from 'next/link'
+import createSession from '../actions/createSession'
 
 const Login = () => {
   return (
     <div className='flex items-center justify-center'>
       <div className='bg-white shadow-lg rounded-lg p-6 w-full max-w-sm mt-20'>
-        <form>
+        <form action={createSession}>
           <h2 className='text-2xl font-bold text-center text-gray-800 mb-6'>Login</h2>
 
           <div className='mb-4'>
