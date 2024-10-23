4742c365634f8b0a77e62bfeb9cdc8c19f285a70
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-10-22 23:16:30 -0700

    Add cookie and session with create admin client logic

--- src/app/actions/createSession.js
+++ src/app/actions/createSession.js
@@ -1,7 +1,12 @@
 'use server'
 
+import { createAdminClient } from '@/config/appwrite'
+import { cookies } from 'next/headers'
+
 async function createSession(previousState, formData) {
   console.log(formData, 'formData')
+
+  console.log(formData.name)
 
   const email = formData.get('email')
   const password = formData.get('password')
@@ -11,6 +16,23 @@ async function createSession(previousState, formData) {
       error: 'Please fill out all fields',
     }
   }
+
+  // Get account instance
+  const { account } = await createAdminClient()
+
+  try {
+    // Generate a session
+    const session = await account.createEmailPasswordSession(email, password)
+
+    // Create cookie
+    cookies().set('appwrite-session', session.secret, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+      expires: new Date(session.expire),
+      path: '/',
+    })
+  } catch (error) {}
 
   return {
     success: true,
