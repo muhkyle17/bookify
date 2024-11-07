012436796de5193a3d6ac0a0e8412c1ae2ed8772
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-11-06 21:55:59 -0800

    Add initial logic for the authprovider

--- context/authContext.js
+++ context/authContext.js
@@ -2,3 +2,30 @@ import { createContext, useContent, useState, useEffect } from 'react'
 import checkAuth from '@/src/app/actions/checkAuth'
 
 const AuthContext = createContext()
+
+export const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const [currentUser, setCurrentUser] = useState(null)
+
+  useEffect(() => {
+    const checkAuthentication = async () => {
+      const { isAuthenticated, user } = await checkAuth()
+      setIsAuthenticated(isAuthenticated)
+      setCurrentUser(user)
+    }
+
+    checkAuthentication()
+  }, [])
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isAuthenticated,
+        setIsAuthenticated,
+        currentUser,
+        setCurrentUser,
+      }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
