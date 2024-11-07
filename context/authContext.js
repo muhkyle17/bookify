b61c4619c625997ae990692be2a9c84ad86c489c
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-11-06 21:57:51 -0800

    Add a useAuth provider

--- context/authContext.js
+++ context/authContext.js
@@ -29,3 +29,13 @@ export const AuthProvider = ({ children }) => {
     </AuthContext.Provider>
   )
 }
+
+export const useAuth = () => {
+  const context = useContext(AuthContext)
+
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider')
+  }
+
+  return context
+}
