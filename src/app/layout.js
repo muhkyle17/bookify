93522e8f24806c5d0c17eb194c6ba5d157daa58a
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-11-06 22:03:25 -0800

    Wrap the auth wrapper around everything in the layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { Toaster } from 'react-hot-toast'
+import AuthWrapper from './components/AuthWrapper'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
@@ -15,13 +16,15 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
-      <body className={inter.className}>
-        <Header />
-        <main className='mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8'>{children}</main>
-        <Toaster />
-        <Footer />
-      </body>
-    </html>
+    <AuthWrapper>
+      <html lang='en'>
+        <body className={inter.className}>
+          <Header />
+          <main className='mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8'>{children}</main>
+          <Toaster />
+          <Footer />
+        </body>
+      </html>
+    </AuthWrapper>
   )
 }
