dfeeb27e481cba25dc9f3318abfb2e0880b92a04
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-11-08 13:14:57 -0800

    Add email and password vaidation logic

--- src/app/actions/createUser.js
+++ src/app/actions/createUser.js
@@ -2,6 +2,31 @@
 import { createAdminClient } from '@/config/appwrite'
 import { ID } from 'node-appwrite'
 
-async function createUser(previousState, formData) {}
+async function createUser(previousState, formData) {
+  const name = formData.get('name')
+  const email = formData.get('email')
+  const password = formData.get('password')
+  const confirmPassword = formData.get('confirm-password')
+
+  if (!email || !name || !password) {
+    return {
+      error: 'Please fill in all fields',
+    }
+  }
+
+  if (password.length < 8) {
+    return {
+      error: 'Password must be at least 8 characters long',
+    }
+  }
+
+  if (password !== confirmPassword) {
+    return {
+      error: "Passwords don't match",
+    }
+  }
+
+  // Get account instance
+}
 
 export default createUser
