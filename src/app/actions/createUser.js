2f487b9e6817e8a300a1dc55497c7efc6f6ec7e6
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-11-08 13:14:28 -0800

    Init create user server file

---
+++ src/app/actions/createUser.js
@@ -0,0 +1,7 @@
+'use server'
+import { createAdminClient } from '@/config/appwrite'
+import { ID } from 'node-appwrite'
+
+async function createUser(previousState, formData) {}
+
+export default createUser
