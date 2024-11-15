9e3c16788836933b1f4a43af21e398aaeeee869c
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-11-14 20:29:41 -0800

    Add initial logic for checking if there's a user

--- src/app/actions/createRoom.js
+++ src/app/actions/createRoom.js
@@ -1,1 +1,24 @@
 'use server'
+
+import { ID } from 'node-appwrite'
+import { revalidatePath } from 'next/cache'
+
+import checkAuth from './checkAuth'
+import { createAdminClient } from '@/config/appwrite'
+
+async function createRoom(previousState, formData) {
+  // Get databases instance
+  const { databases } = await createAdminClient()
+
+  try {
+    const { user } = await checkAuth()
+
+    if (!user) {
+      return {
+        error: 'You must be logged in to create a room.',
+      }
+    }
+  } catch (error) {}
+}
+
+export default createRoom
