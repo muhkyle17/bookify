058930405f02e8af1539a7fe514a6982f929c063
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-11-21 14:33:27 -0800

    Add logic for getting the bookings of a single user

--- src/app/actions/getMyBookings.js
+++ src/app/actions/getMyBookings.js
@@ -5,32 +5,40 @@ import { Query } from 'node-appwrite'
 import { redirect } from 'next/navigation'
 
 import { createSessionClient } from '@/config/appwrite'
+import checkAuth from './checkAuth'
 
-async function getMyRooms() {
+async function getMyBookings() {
   const sessionCookie = cookies().get('appwrite-session')
   if (!sessionCookie) {
     redirect('/login')
   }
 
   try {
-    const { account, databases } = await createSessionClient(sessionCookie.value)
+    const { databases } = await createSessionClient(sessionCookie.value)
 
     // Get user's ID
-    const user = await account.get()
-    const userID = user.$id
+    const { user } = await checkAuth()
 
-    // Fetch user's rooms
-    const { documents: rooms } = await databases.listDocuments(
+    if (!user) {
+      return {
+        error: 'You must be logged in to view bookings',
+      }
+    }
+
+    // Fetch user's bookings
+    const { documents: bookings } = await databases.listDocuments(
       process.env.NEXT_PUBLIC_APPWRITE_DATABASE,
-      process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ROOMS,
-      [Query.equal('user_id', userID)]
+      process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_BOOKINGS,
+      [Query.equal('user_id', user.id)]
     )
 
-    return rooms
+    return bookings
   } catch (error) {
-    console.log("Failed to get user's rooms", error)
-    redirect('/error')
+    console.log("Failed to get user's bookings", error)
+    return {
+      error: 'Failed to get bookings',
+    }
   }
 }
 
-export default getMyRooms
+export default getMyBookings
