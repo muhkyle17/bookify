c9c3fd4bc6434389c41a84ff3e8ad3f893783170
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-11-21 21:06:33 -0800

    Display initial data without proper ui

--- src/app/bookings/page.jsx
+++ src/app/bookings/page.jsx
@@ -1,5 +1,18 @@
-const Bookings = () => {
-  return <div>Bookings</div>
+import Heading from '../components/Heading'
+import getMyBookings from '../actions/getMyBookings'
+
+const Bookings = async () => {
+  const bookings = await getMyBookings()
+
+  return (
+    <>
+      {bookings.length === 0 ? (
+        <p className='text-gray-600 mt-4'>You have no bookings</p>
+      ) : (
+        bookings.map(booking => <h3>{booking.room_id.name}</h3>)
+      )}
+    </>
+  )
 }
 
 export default Bookings
---
+++ src/app/components/BookedRoomCard.jsx
@@ -0,0 +1,5 @@
+const BookedRoomCard = ({ booking }) => {
+  return <div>Booked Room Card</div>
+}
+
+export default BookedRoomCard
