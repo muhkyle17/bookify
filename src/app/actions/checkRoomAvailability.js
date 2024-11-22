41cc275dc39bdde3447c53859ddebe047208d432
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-11-22 15:39:19 -0800

    Add logic for checking room availability

--- src/app/actions/checkRoomAvailability.js
+++ src/app/actions/checkRoomAvailability.js
@@ -3,34 +3,64 @@
 import { cookies } from 'next/headers'
 import { Query } from 'node-appwrite'
 import { redirect } from 'next/navigation'
+import { DateTime } from 'luxon'
 
 import { createSessionClient } from '@/config/appwrite'
 
-async function getMyRooms() {
+// Convert date string to a Luxon DateTime object in UTC
+function toUTCDateTime(dateString) {
+  return DateTime.fromISO(dateString, { zone: 'utc' }).toUTC
+}
+
+// Check for overlapping date ranges
+function dateRangesOverlap(checkInA, checkOutA, checkInB, checkOutB) {
+  return checkInA < checkOutB && checkOutA > checkInB
+}
+
+async function checkRoomAvailability(roomId, checkIn, checkOut) {
   const sessionCookie = cookies().get('appwrite-session')
   if (!sessionCookie) {
     redirect('/login')
   }
 
   try {
-    const { account, databases } = await createSessionClient(sessionCookie.value)
+    const { databases } = await createSessionClient(sessionCookie.value)
 
-    // Get user's ID
-    const user = await account.get()
-    const userID = user.$id
+    const checkInDateTime = toUTCDateTime(checkIn)
+    const checkOutDateTime = toUTCDateTime(checkOut)
 
-    // Fetch user's rooms
-    const { documents: rooms } = await databases.listDocuments(
+    // Fetch all bookings for a given room
+    const { documents: bookings } = await databases.listDocuments(
       process.env.NEXT_PUBLIC_APPWRITE_DATABASE,
-      process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ROOMS,
-      [Query.equal('user_id', userID)]
+      process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_BOOKINGS,
+      [Query.equal('user_id', roomId)]
     )
 
-    return rooms
+    // Loop over bookings and check for overlaps
+    for (const booking of bookings) {
+      const bookingCheckInDateTime = toUTCDateTime(booking.check_in)
+      const bookingCheckOutDateTime = toUTCDateTime(booking.check_out)
+
+      if (
+        dateRangesOverlap(
+          checkInDateTime,
+          checkOutDateTime,
+          bookingCheckInDateTime,
+          bookingCheckOutDateTime
+        )
+      ) {
+        return false // Overlap found, do not book
+      }
+    }
+
+    // No overlap found, continue to book
+    return true
   } catch (error) {
-    console.log("Failed to get user's rooms", error)
-    redirect('/error')
+    console.log('Failed to check availability', error)
+    return {
+      error: 'Failed to check availability',
+    }
   }
 }
 
-export default getMyRooms
+export default checkRoomAvailability
